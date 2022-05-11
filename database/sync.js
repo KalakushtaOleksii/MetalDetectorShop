c3de1ededdec86b2f5812eec4aa8dc464a64dff4
Льоша <kalakushta@ukr.net>
2022-05-12 00:23:10 +0300

    app sync

--- database/sync.js
+++ database/sync.js
@@ -1,17 +1,35 @@
-const models = require('./models')
+const Cart = require("./models/cart.model");
+const CartItem = require("./models/cart-item.model");
+const Country = require("./models/country.model");
+const Frequency = require("./models/frequency.model");
+const FrequencyList = require("./models/frequency-list.model");
+const Order = require("./models/order.model");
+const OrderDetail = require("./models/order-detail.model");
+const Product = require("./models/product.model");
+const ProductAttribute = require("./models/product-attribute.model");
+const ProductCategory = require("./models/product-category.model");
+const ProductPrice = require("./models/product-price.model");
+const Storage = require("./models/storage.model");
+const StorageProductList = require("./models/storage-product-list.model");
+const Trademark = require("./models/trademark.model");
+const User = require("./models/user.model");
 
-models.Cart.sync()
-models.User.sync()
-models.Order.sync()
-models.Product.sync()
-models.ProductAttribute.sync()
-models.Storage.sync()
-models.ProductPrice.sync()
-models.Frequency.sync()
-models.FrequencyList.sync()
-models.Order.sync()
-models.OrderDetail.sync()
-models.Trademark.sync()
-models.Country.sync()
-models.StorageProductList.sync()
-models.ProductCategory.sync()
+;(
+    async () => {
+        await Cart.sync()
+        await CartItem.sync()
+        await Country.sync()
+        await Frequency.sync()
+        await FrequencyList.sync()
+        await Order.sync()
+        await OrderDetail.sync()
+        await Product.sync()
+        await ProductAttribute.sync()
+        await ProductCategory.sync()
+        await ProductPrice.sync()
+        await Storage.sync()
+        await StorageProductList.sync()
+        await Trademark.sync()
+        await User.sync()
+    }
+)()
